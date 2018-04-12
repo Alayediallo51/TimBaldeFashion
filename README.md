7e4fe32f23d246eee66f17ca347c4991e6f705aa
Abdoulaye <stageinfo13sgmail.com>
2018-04-12 05:20:10 -1200

    integrer le template de base dans un projet de symfony

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TimBaldeFashion
+projetTima
+==========
+
+A Symfony project created on April 12, 2018, 1:14 pm.
---
+++ src/tim/BaldeFashionBundle/timBaldeFashionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace tim\BaldeFashionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class timBaldeFashionBundle extends Bundle
+{
+}
